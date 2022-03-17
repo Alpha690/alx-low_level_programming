f203a40ef8eeca00e60f431eff775acffabbe452
Alpha690 <manumuuo@gmail.com>
2022-03-17 23:12:46 +0300

    1

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,23 +1,28 @@
-#include "main.h"
-/**
- * print_line - drew a straight line
- * @n: number of times underscore is printed
- * Description: can only use _putchar to print
- */
-void print_line(int n)
-{
-if (n <= 0)
-{
-_putchar('\n');
-}
-else
-{
-int i;
+#include "holberton.h"
 
-for (i = 1; i <= n; i++)
+/**
+ * print_diagonal - draws a diagonal line on the terminal
+ * @n: number of times the character \ should be printed
+ */
+void print_diagonal(int n)
 {
-_putchar('_');
-}
-_putchar('\n');
-}
+	if (n <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		int i, j;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
 }
