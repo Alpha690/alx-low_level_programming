4c1db6acf6da94cea853ec64a3602e933183f601
Alpha690 <manumuuo@gmail.com>
2022-03-30 15:35:53 +0300

    1

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,4 @@
-#include "MAIN.H"
+#include "main.h"
 
 /**
  * _puts_recursion - prints a string, followed by a new line
--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,4 +1,4 @@
-#include "MAIN.H"
+#include "main.h"
 
 /**
  * _print_rev_recursion - Print a string in reverse.
