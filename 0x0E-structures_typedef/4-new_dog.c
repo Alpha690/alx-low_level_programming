54f62468da9e2c9f6ab8a5c1893e5865eaa47708
Alpha690 <manumuuo@gmail.com>
2022-04-11 16:42:23 +0300

    fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,83 +1,98 @@
-#include "dog.h"
 #include <stdlib.h>
-
-int _strlen(char *str);
-char *_strcopy(char *dest, char *src);
-dog_t *new_dog(char *name, float age, char *owner);
-
+#include "dog.h"
+#include <string.h>
 /**
- * _strlen - Finds the length of a string.
- * @str: The string to be measured.
+ * _strlen - returns the length of a string
  *
- * Return: The length of the string.
+ * @s: the string we are counting the characters of
+ * Return: the length of the string
  */
-int _strlen(char *str)
+int _strlen(char *s)
 {
-	int len = 0;
+	int length;
 
-	while (*str++)
-		len++;
+	length = 0;
 
-	return (len);
+	while (*s != '\0')
+	{
+		s++;
+		length++;
+	}
+	return (length);
 }
 
 /**
- * _strcopy - Copies a string pointed to by src, including the
- *            terminating null byte, to a buffer pointed to by dest.
- * @dest: The buffer storing the string copy.
- * @src: The source string.
- *
- * Return: The pointer to dest.
+ * _strdup - Returns a pointer to a newly allocated space in memory, which
+ * contains a copy of the string given as a parameter.
+ * @str: the string to copy
+ * Return: the address of the copied string, NULL if failed
  */
-char *_strcopy(char *dest, char *src)
+
+char *_strdup(char *str)
 {
-	int index = 0;
+	unsigned int x = 0;
+	unsigned int size = 0;
+	char *newString;
 
-	for (index = 0; src[index]; index++)
-		dest[index] = src[index];
+	if (str == NULL)
+		return (NULL);
 
-	dest[index] = '\0';
+	while (*(str + size))
+		size++;
 
-	return (dest);
+	newString = malloc(size * sizeof(char) + 1);
+
+	if (newString == NULL)
+	{
+		free(newString);
+		return (NULL);
+	}
+
+	while (x < size)
+	{
+		*(newString + x) = *(str + x);
+		x++;
+	}
+	*(newString + x) = '\0';
+	return (newString);
 }
 
 /**
- * new_dog - Creates a new dog.
- * @name: The name of the dog.
- * @age: The age of the dog.
- * @owner: The owner of the dog.
- *
- * Return: The new struct dog.
+ * new_dog - this creates a struct with the data to make a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: the name of the dog's owner
+ * Return: returns a pointer to the new dog struct
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *doggo;
 
-	if (name == NULL || age < 0 || owner == NULL)
-		return (NULL);
+	dog_t *newdog = malloc(sizeof(struct dog));
 
-	doggo = malloc(sizeof(dog_t));
-	if (doggo == NULL)
-		return (NULL);
-
-	doggo->name = malloc(sizeof(char) * (_strlen(name) + 1));
-	if (doggo->name == NULL)
+	if (newdog == NULL)
 	{
-		free(doggo);
+		free(newdog);
+		return (NULL);
+	}
+	newdog->name = _strdup(name);
+
+	if (newdog->name == NULL)
+	{
+		free(newdog->name);
+		free(newdog);
 		return (NULL);
 	}
 
-	doggo->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
-	if (doggo->owner == NULL)
+	newdog->owner = _strdup(owner);
+
+	if (newdog->owner == NULL)
 	{
-		free(doggo->name);
-		free(doggo);
+		free(newdog->name);
+		free(newdog->owner);
+		free(newdog);
 		return (NULL);
 	}
 
-	doggo->name = _strcopy(doggo->name, name);
-	doggo->age = age;
-	doggo->owner = _strcopy(doggo->owner, owner);
-
-	return (doggo);
+	newdog->age = age;
+	return (newdog);
 }
--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,16 +1,19 @@
 #include "dog.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * free_dog - Frees dogs.
- * @d: The dog to be freed.
+ * free_dog - free dogs
+ * @d: who I am freeing
+ *
+ * Return: void
  */
 void free_dog(dog_t *d)
 {
-	if (d == NULL)
-		return;
-
-	free(d->owner);
-	free(d->name);
-	free(d);
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
